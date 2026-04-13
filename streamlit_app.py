83e7aef376446bfc625a506c8b3ef4719d3baf71
akanksha <142966938+Akankshaaa09@users.noreply.github.com>
2026-04-13 15:19:13 +0200

    Refactor imports and improve index handling

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import cohere
 import os
-from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_community.vectorstores import FAISS
 from langchain_huggingface import HuggingFaceEmbeddings
 import warnings
